470b4651c2bc1e16c26a0ef297969215a45f09d1
mehne <mehne@DESKTOP-UETTQ6O>
2022-06-03 22:40:25 +0200

    langueg-vm yea

---
+++ cmake-build-debug/CMakeFiles/langueg_vm.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/dev/C/langueg-vm/lala-load/lalafile.c" "C:/dev/C/langueg-vm/cmake-build-debug/CMakeFiles/langueg_vm.dir/lala-load/lalafile.c.obj"
+  "C:/dev/C/langueg-vm/main.c" "C:/dev/C/langueg-vm/cmake-build-debug/CMakeFiles/langueg_vm.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/langueg_vm.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/langueg_vm.dir/lala-load/lalafile.c.obj"
+  "CMakeFiles/langueg_vm.dir/main.c.obj"
+  "langueg_vm.exe"
+  "langueg_vm.exe.manifest"
+  "langueg_vm.pdb"
+  "liblangueg_vm.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/langueg_vm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
